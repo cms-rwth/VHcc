dfa73e6250de35b26eb3ba81783287329192f073
Andrey Pozdnyakov <andreypz@gmail.com>
2022-12-02 12:56:49 +0100

    Update readme

--- cfg_VHcc.py
+++ cfg_VHcc.py
@@ -9,13 +9,27 @@ from VHcc.workflows.Zll_process import (
 cfg = {
     "userconfig": {'version':'test_nolepsf'},
     "dataset": {
-        "jsons": ["src/VHcc/metadata/mcsamples_2017_vjets_Zll_used_nonCorruptedOnly.json"],
+        "jsons": ["src/VHcc/metadata/mcsamples_2017_vjets_Zll_used_nonCorruptedOnly.json",
+                  "src/VHcc/metadata/mcsamples_2017_vjets_ext_Zll_used_nonCorruptedOnly.json",
+                  "src/VHcc/metadata/mcsamples_2017_other_Zll_used_nonCorruptedOnly.json",
+                  "src/VHcc/metadata/mcsamples_2017_higgs_Zll_used.json",
+                  "src/VHcc/metadata/datasamples_2017_Zll_used.json"],
         "campaign": "UL17",
         "year": "2017",
         "filter": {
             "samples": [
                 "DYJetsToLL_nlo",
                 #"DY1ToLL_PtZ-250To400",
+                #"DY1ToLL_PtZ-50To150",
+                #"DY1ToLL_PtZ-150To250",
+                #"DY1ToLL_PtZ-400ToInf",
+                #"DY2ToLL_PtZ-50To150",
+                #"DY2ToLL_PtZ-150To250",
+                #"DY2ToLL_PtZ-250To400",
+                #"DY2ToLL_PtZ-400ToInf",
+                #"",
+                #"",
+                #"",
             ],
             "samples_exclude": [],
         },
@@ -32,7 +46,7 @@ cfg = {
         "mem_per_worker": 2,  # GB
         "chunk": 50000,
         "max": None,
-        "skipbadfiles": None,
+        "skipbadfiles": True,
         "voms": None,
         "limit": 1,
         "retries": 20,
